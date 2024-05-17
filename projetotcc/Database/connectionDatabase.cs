6e88ee1af0b4ccea7f4768d26ea0d5f50205c1fb
Joao Pedro Chapichi <chapichijp@gmail.com>
2024-05-17 20:04:51 -0300

    Alterações - Criado a pasta Database para inserir o codigo do banco e a classe com o metodo de conexao.

---
+++ projetotcc/Database/connectionDatabase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace projetotcc.Database
+{
+    internal class connectionDatabase
+    {
+        // Conexão com o banco de dados
+        // Declaração dos parametros para conexao
+        string connectionString = "Host=postgres;Port=5432;Username=postgres;Password=123456;Database=pontofacil_db";
+        NpgsqlConnection connection = null;
+
+        // Metodo de conexao
+        public NpgsqlConnection connectionDB()
+        {
+            try
+            {
+                connection = new NpgsqlConnection(connectionString);
+                connection.Open();
+                return connection;
+            } 
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+    }
+}
